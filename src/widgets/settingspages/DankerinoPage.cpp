0e490a0d6a1f49dc227812638540b2a634e39ce2
felyp <janichfilip@seznam.cz>
2023-12-20 06:42:46 +0100

    Lower minimum 'High rate limit spam delay' value to 45ms.

--- src/widgets/settingspages/DankerinoPage.cpp
+++ src/widgets/settingspages/DankerinoPage.cpp
@@ -66,7 +66,7 @@ void DankerinoPage::initLayout(GeneralPageView &layout)
     //layout.addCheckbox("Enable loading 7TV emotes", s.enableLoadingSevenTV);
     layout.addTitle("Miscellaneous");
     layout.addIntInput("High rate limit spam delay in milliseconds (mod/vip)",
-                       s.twitchHighRateLimitDelay, 100, 2000, 100);
+                       s.twitchHighRateLimitDelay, 45, 2000, 100);
     layout.addIntInput(
         "Low rate limit spam delay in milliseconds (non mod/vip)",
         s.twitchLowRateLimitDelay, 500, 3000, 1100);
